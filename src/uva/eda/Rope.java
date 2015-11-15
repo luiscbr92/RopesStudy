507e35a050d1b8b5ad0f007384dca9abf53f36cc
luiscbr92 <luiscbr92@gmail.com>
2015-11-15 14:17:05 +0100

    Ropes interface done. Next to do: implement

--- src/uva/eda/Rope.java
+++ src/uva/eda/Rope.java
@@ -1,61 +1,103 @@
 package uva.eda;
 
+
+/**
+ * Implements texts Strings as Ropes
+ * @author Luis Alberto Centeno Bragado, Ángel Posada García
+ *
+ */
 public class Rope {
-
-	private String subcadena;
-	private Rope nodoI;
-	private Rope nodoD;
-	private int tamaño;
-	private int profundidad;
+	private String subString;
+	private Rope left;
+	private Rope right;
+	private int length;
+	private int depth;
 
 	/**
-	 * Crea un nodo de tipo hoja.
-	 * @param texto
-	 * @param longitud
+	 * Initializes a new Rope with a String
+	 * @param text
 	 */
-	public Rope(String texto, int longitud){
-		subcadena = texto;
-		tamaño = longitud;
+	public Rope(String text){
+		// depth = 0;
 	}
 	
 	/**
-	 * Crea un nodo interno, con referencias a nuevos nodos internos.
-	 * @param nodoI
-	 * @param nodoD
-	 * @param n
-	 * @param h
+	 * Initializes a new Rope joining two Ropes given
+	 * @param left
+	 * @param right
 	 */
-	public Rope(Rope nodoI, Rope nodoD, int n, int h){
-		this.nodoI = nodoI;
-		this.nodoD = nodoD;
-		tamaño = n;
-		profundidad = h;
-	}
-	
-	public String getsubcadena(Rope nodo){
-		return subcadena;
-	}
-	
-	public Rope getnodoI(Rope nodo){
-		return nodoI;
-	}
-	
-	public Rope getnodoD(Rope nodo){
-		return nodoD;
-	}
-	
-	public int gettamaño(Rope nodo){
-		return tamaño;
-	}
-	
-	public int getprofundidad(Rope nodo){
-		return profundidad;
-	}
-	
-	public Rope concatenar(Rope nodoI, Rope nodoD){
+	public Rope(Rope left, Rope right){
 		
-		//return new Rope(nodoI, nodoD);
-		return nodoI;
 	}
+	
+	/**
+	 * USE WHEN DEPTH=0!! Returns the substring of this Rope
+	 * @return
+	 */
+	public String getSubString(){
+		return subString;
+	}
+	
+	/**
+	 * USE WHEN DEPTH>0!! Returns the left Rope of this Rope
+	 * @return
+	 */
+	public Rope getLeft(){
+		return left;
+	}
+
+	/**
+	 * USE WHEN DEPTH>0!! Returns the right Rope of this Rope
+	 * @return
+	 */
+	public Rope getRight(){
+		return right;
+	}
+	
+	/**
+	 * Returns the length of this Rope
+	 * @return
+	 */
+	public int length(){
+		return length;
+	}
+	
+	/**
+	 * Returns the depth of this Rope
+	 * @return
+	 */
+	public int depth(){
+		return depth;
+	}
+	
+	/**
+	 * Concatenates two Ropes by putting the Rope given on the left of this Rope
+	 * @param left
+	 * @return
+	 */
+	public Rope concatenateLeft(Rope left){
+		return new Rope(left, this);
+	}
+	
+	/**
+	 * Concatenates two Ropes by putting the Rope given on the right of this Rope
+	 * @param right
+	 * @return
+	 */
+	public Rope concatenateRight(Rope right){
+		return new Rope(this, right);
+	}
+	
+	/**
+	 * Returns the character allocated in the position given
+	 * @param position
+	 * @return
+	 */
+	public char getChar(int position){
+		return 'a'; // CHANGE THIS
+	}
+	
+	
+	// BALANCE: public or private method
 	
 }
