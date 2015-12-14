b896170c937636d5e72bdcd177b691d262bbbcbc
luiscbr92 <luiscbr92@gmail.com>
2015-12-14 13:02:09 +0100

    Finished

--- src/uva/eda/Rope.java
+++ src/uva/eda/Rope.java
@@ -6,6 +6,7 @@ import java.util.Stack;
 
 /**
  * Implements texts Strings as Ropes
+ * 
  * @author Luis Alberto Centeno Bragado, Ángel Posada García
  *
  */
@@ -19,128 +20,130 @@ public class Rope {
 	/**
 	 * Initializes a new Rope with a String
 	 */
-	public Rope(String text){
+	public Rope(String text) {
 		subString = text;
 		length = text.length();
 		depth = 0;
 	}
-	
+
 	/**
 	 * Initializes a new Rope joining two Ropes given
 	 */
-	public Rope(Rope left, Rope right){
+	public Rope(Rope left, Rope right) {
 		this.left = left;
 		this.right = right;
 		length = left.length() + right.length();
-		if(left.depth() > right.depth())
+		if (left.depth() > right.depth())
 			depth = left.depth() + 1;
 		else
 			depth = right.depth() + 1;
 	}
-	
+
 	/**
 	 * USE WHEN DEPTH=0!! Returns the substring of this Rope
 	 */
-	public String getSubString(){
+	public String getSubString() {
 		return subString;
 	}
-	
+
 	/**
 	 * USE WHEN DEPTH>0!! Returns the left Rope of this Rope
 	 */
-	public Rope getLeft(){
+	public Rope getLeft() {
 		return left;
 	}
 
 	/**
 	 * USE WHEN DEPTH>0!! Returns the right Rope of this Rope
 	 */
-	public Rope getRight(){
+	public Rope getRight() {
 		return right;
 	}
-	
+
 	/**
 	 * Returns the length of this Rope
 	 */
-	public int length(){
+	public int length() {
 		return length;
 	}
-	
+
 	/**
 	 * Returns the depth of this Rope
 	 */
-	public int depth(){
+	public int depth() {
 		return depth;
 	}
-	
+
 	/**
 	 * Checks if this rope has depth 0.
 	 */
-	public boolean isLeaf(){
+	public boolean isLeaf() {
 		return depth() == 0;
 	}
-	
+
 	/**
-	 * Concatenates two Ropes by putting the Rope given on the right of this Rope
+	 * Concatenates two Ropes by putting the Rope given on the right of this
+	 * Rope
 	 */
-	public Rope concatenate(Rope right){
+	public Rope concatenate(Rope right) {
 		return new Rope(this, right);
 	}
-	
+
 	/**
 	 * Returns the character allocated in the position given
 	 */
-	public char getChar(int position){
-		if(isLeaf())
+	public char getChar(int position) {
+		if (isLeaf())
 			return getSubString().charAt(position);
-		else{
-			if(position < getLeft().length())
+		else {
+			if (position < getLeft().length())
 				return getLeft().getChar(position);
-			else{
+			else {
 				position -= getLeft().length();
 				return getRight().getChar(position);
 			}
 		}
 	}
-	
+
 	/**
 	 * Returns the Rope perfectly balanced
 	 */
-	public Rope balance(){
+	public Rope balance() {
 		Stack<Rope> myStack = new Stack<Rope>();
 		List<Rope> myList = new ArrayList<Rope>();
 		Rope tmp;
-		
+
 		myStack.push(this);
-		while(!myStack.empty()){
+		while (!myStack.empty()) {
 			tmp = myStack.pop();
-			if(tmp.isLeaf())
+			if (tmp.isLeaf())
 				myList.add(tmp);
-			else{
+			else {
 				myStack.push(tmp.getRight());
 				myStack.push(tmp.getLeft());
 			}
 		}
-		
+
 		return getTree(myList);
 	}
-	
+
 	/**
 	 * Returns this rope as String
 	 */
-	public String toString(){
-		if(isLeaf())
+	public String toString() {
+		if (isLeaf())
 			return getSubString();
 		else
 			return getLeft().toString() + getRight().toString();
 	}
-	
-	private Rope getTree(List<Rope> original){
-		if(original.size() == 1)
+
+	private Rope getTree(List<Rope> original) {
+		if (original.size() == 1)
 			return original.get(0);
-		else{
-			return new Rope(getTree(original.subList(0, original.size()/2)), getTree(original.subList(original.size()/2, original.size())));
+		else {
+			return new Rope(getTree(original.subList(0, original.size() / 2)),
+					getTree(original.subList(original.size() / 2, original.size())));
 		}
 	}
-	
+
 }
